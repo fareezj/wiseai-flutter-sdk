9c2efc63f78415a78df338fd741030fdf3d3ca34
Fareez <fareez.jasmi@myeg.com.my>
2026-06-24 10:45:27 +0800

    -

--- example/lib/mykad_result_page.dart
+++ example/lib/mykad_result_page.dart
@@ -50,9 +50,8 @@ class _MyKadResultPageState extends State<MyKadResultPage> {
             //extraParam: extraParam,
           )
         : IosMyKadEkycConfig(
-            apiToken:
-                "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ3aXNlYWkiLCJzdWIiOiJ3aXNlYWktYXBpIiwiaWQiOiIzNzIiLCJubSI6Ik15S2FkIGVLWUMiLCJtaXNjIjoiIiwidmVyIjozfQ.fVufFcKGWzfrpIz2QJZMIQ0qXqwlw_IjJpnYz5c4jkU",
-            apiURL: "https://wiseconsole-demo.wiseai.tech/",
+            apiToken: "",
+            apiURL: "",
             language: widget.language,
             isEncrypt: false,
             isExportFace: false,
